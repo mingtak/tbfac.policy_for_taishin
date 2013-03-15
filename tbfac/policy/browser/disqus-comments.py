3ea6b98263c0e6021ed47f0a8a8ceaaf9e063277
TsungWei Hu <marr.tw@gmail.com>
2013-03-15 11:51:28 +0800

    Add Disqus Recent Comments Browser View.

---
+++ tbfac/policy/browser/disqus-comments.py
@@ -0,0 +1,27 @@
+from Products.Five.browser import BrowserView
+from ..util import getLatestComments
+
+
+class RecentCommentsView(BrowserView):
+
+    def getComments(self):
+        """Returns list of latest comments"""
+        comments = []
+        posts, threads = getLatestComments(self.context)
+        for c in posts:
+            thread_id = c.get('thread', '')
+            thread = {}
+            if thread_id and thread_id in threads:
+                thread = {
+                    'title': threads[thread_id].get('title', 'No Title'),
+                    'url': threads[thread_id].get('link', ''),
+                }
+            comments.append({
+                'author': c.get('author', {}).get('name', 'Anonymous'),
+                'body': c.get('raw_message', '')[:30],
+                'thread': thread,
+                'url': '%s#comment-%s' % (thread.get('url', ''), c.get('id',
+                    '')),
+            })
+        return comments
+
